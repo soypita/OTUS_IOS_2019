f479f6cdf59c0b10fbfcdc676ac887e52e180b4f
Дмитрий Сазонов <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2019-08-28 15:04:25 +0300

    init commit

---
+++ Homework1/Homework1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by Дмитрий Сазонов on 28/08/2019.
+//  Copyright © 2019 Дмитрий Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
