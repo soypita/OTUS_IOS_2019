5f2030f0bbd2f144b4e4b911b346e816d799481f
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2019-09-13 22:10:30 +0300

    feat: add second pard with collection and timers

--- Homework1/Homework1/Controllers/BenchMarkViewController.swift
+++ Homework1/Homework1/Controllers/BenchMarkViewController.swift
@@ -9,21 +9,73 @@
 import UIKit
 
 class BenchMarkViewController: UIViewController {
-
+    private let reuseIdentifier = "customCellId"
+    private let reuseIdForStackCell = "cellWithStack"
+    
+    var timers: [TimerState] = []
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    lazy var componentsFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addBehaviors(behaviors: [TimerControllerBehavior()])
+        
+        for _ in 1...10 {
+            timers.append(TimerState())
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        for timerItem in timers {
+            timerItem.resetTimer()
+        }
     }
-    */
+}
+extension BenchMarkViewController {
+    @objc func runScheduledTask(_ runningTimer: Timer) {
+        timers[runningTimer.userInfo as! Int].updateTime()
+        DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+        }
+    }
+}
+// MARK: - UICollectionViewDataSource
+extension BenchMarkViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return timers.count + 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Последняя ячейка отличается от всех остальных
+        if indexPath.row == timers.count {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdForStackCell,
+                                                          for: indexPath) 
+            return cell
+        }
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath) as! CustomCell
+        cell.backgroundColor = .blue
+        if timers[indexPath.row].timer == nil {
+            timers[indexPath.row].timer = (Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.runScheduledTask), userInfo: indexPath.row, repeats: true))
+        }
+        cell.timerLabel.text = timers[indexPath.row].currentTimeLabel
+        return cell
+    }
+}
 
+// MARK: - UICollectionViewDelegate
+extension BenchMarkViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //Все ячейки кроме последней работают как таймеры
+        if indexPath.row != timers.count {
+           timers[indexPath.row].changeTimerState()
+        }
+    }
 }
